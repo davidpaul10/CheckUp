841650aa20c239d26211ecd6486f907c8bc292a0
unknown <pda.banda@yavirac.edu.ec>
2026-07-28 10:02:03 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Appointments } from './appointments/appointments';
+import { GreenPass } from './green-pass/green-pass';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'appointments', component: Appointments },
+  { path: 'green-pass', component: GreenPass },
+  { path: 'profile', component: Profile },
+  { path: '**', redirectTo: 'home' },
+];
+
---
+++ src/app/green-pass/green-pass.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-green-pass',
+  imports: [],
+  templateUrl: './green-pass.html',
+  styleUrl: './green-pass.css',
+})
+export class GreenPass {}
